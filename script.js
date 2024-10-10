5d547eee462a19310d9f1cfaaad7d80e274d41d0
yoann <yoann.ochietti@gmail.com>
2024-10-10 12:48:31 -0400

    Create operate function

--- script.js
+++ script.js
@@ -2,9 +2,20 @@ let leftOperand;
 let rightOperand;
 let operator;
 
-
-
-
+function operate(left, right, operation){
+    switch (operation){
+        case "+" :
+            return add(left, right);
+        case "-" :
+            return subtract(left, right);
+        case "*" :
+            return multiply(left, right);
+        case "/" :
+            return divide(left, right);
+        default:
+            throw new Error("Unsupported operation");
+    }
+}
 
 function add(a, b) {
     return a + b;
