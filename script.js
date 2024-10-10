15193a1ac63dfc02eb84a0ee6b0196562447d983
yoann <yoann.ochietti@gmail.com>
2024-10-10 14:07:16 -0400

    Display input on calculator display

--- script.js
+++ script.js
@@ -1,16 +1,66 @@
-let leftOperand;
-let rightOperand;
-let operator;
+let leftOperand = null;
+let rightOperand = null;
+let operator = null;
 
-function operate(left, right, operation){
-    switch (operation){
-        case "+" :
+let display = document.querySelector(".display");
+let displayContent = "";
+
+let calculator = document.querySelector(".calculator");
+calculator.addEventListener("click", handleClick);
+
+function handleClick(clickEvent) {
+    console.log(clickEvent);
+    let content = clickEvent.target.textContent;
+    switch (clickEvent.target.className) {
+        case "num-button":
+            numInput(+content);
+            break;
+        case "decimal-button":
+            decimalInput();
+            break;
+        case "operation-button":
+            operationInput(content);
+            break;
+        case "equal-button":
+            equalInput();
+            break;
+        case "clear-button":
+            clearInput();
+            break;
+        case "back-button":
+            backInput();
+            break;
+        default:
+            throw new Error("Unsupported button clicked");
+    }
+}
+
+function numInput(num) {
+    displayContent += num;
+    updateDisplay();
+}
+
+function decimalInput() {
+    if(displayContent.includes(".")){
+        return;
+    }
+    displayContent += ".";
+    updateDisplay();
+}
+
+function updateDisplay() {
+    display.textContent = displayContent;
+}
+
+function operate(left, right, operation) {
+    switch (operation) {
+        case "+":
             return add(left, right);
-        case "-" :
+        case "-":
             return subtract(left, right);
-        case "*" :
+        case "*":
             return multiply(left, right);
-        case "/" :
+        case "/":
             return divide(left, right);
         default:
             throw new Error("Unsupported operation");
